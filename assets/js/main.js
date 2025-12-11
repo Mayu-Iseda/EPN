c58c5761eaa6661d9edd30a648ee36823afac68a
Mayu Iseda <is12141@inouesada.co.jp>
2025-12-12 08:46:51 +0900

    ハンバーガーメニュー構築

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+{
+  // ハンバーガーメニュー
+  document.addEventListener('DOMContentLoaded', function () {
+    const nav = document.getElementById('headerNav');
+    const btn = document.querySelector('.header-btn');
+    const mask = document.getElementById('mask');
+    const openClass = 'open';
+
+    // menu open / close
+    btn.addEventListener('click', function () {
+      nav.classList.toggle(openClass);
+    });
+
+    // mask close
+    mask.addEventListener('click', function () {
+      nav.classList.remove(openClass);
+    });
+  });
+}
