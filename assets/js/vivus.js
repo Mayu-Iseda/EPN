2685a7634f70aac162af515d88a6836508e33c17
Mayu Iseda <is12141@inouesada.co.jp>
2025-12-15 17:29:21 +0900

    タイトル周り、information編集

--- assets/js/vivus.js
+++ assets/js/vivus.js
@@ -1,4 +1,12 @@
 // SVGアニメーション
 {
-  new Vivus('ttl_anime', { type: 'scenario-sync', duration: 8, forceRender: false, animTimingFunction: Vivus.EASE })
+  window.onload = () => {
+    new Vivus('ttl_anime', {
+      type: 'scenario-sync',
+      duration: 10,
+      forceRender: false,
+      animTimingFunction: Vivus.EASE,
+      start: 'autostart'
+    })
+  }
 }
