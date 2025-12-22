712e65860ecb40d75e0be0f6f714956136ece814
Mayu Iseda <is12141@inouesada.co.jp>
2025-12-22 17:20:39 +0900

    レスポンシブ構築

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,4 +16,34 @@
       nav.classList.remove(openClass);
     });
   });
+
+  document.querySelectorAll('.nav-menu a[href]').forEach(function (el) {
+    el.addEventListener('click', function (event) {
+      document.querySelector('.nav-btn').click();
+    });
+  });
+
+  // ふわっと表示
+  document.addEventListener('DOMContentLoaded', () => {
+    const targets = document.querySelectorAll('.js-fade');
+
+    const options = {
+      root: null,
+      rootMargin: '0px 0px -10% 0px',
+      threshold: 0
+    };
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('is-active');
+        } else {
+          entry.target.classList.remove('is-active');
+        }
+      });
+    }, options);
+
+    targets.forEach(target => observer.observe(target));
+  });
+
 }
